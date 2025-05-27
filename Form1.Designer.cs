53f155bd40a59f0cff1841505834dab50b6eb080
petro <petro@DESKTOP-M8KSNI2>
2025-05-27 20:43:08 +0800

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace PolySolve
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxQuadratic = new System.Windows.Forms.GroupBox();
+            this.labelQuadraticResult = new System.Windows.Forms.Label();
+            this.buttonSolveQuadratic = new System.Windows.Forms.Button();
+            this.textBoxCQuadratic = new System.Windows.Forms.TextBox();
+            this.labelCQuadratic = new System.Windows.Forms.Label();
+            this.textBoxBQuadratic = new System.Windows.Forms.TextBox();
+            this.labelBQuadratic = new System.Windows.Forms.Label();
+            this.textBoxAQuadratic = new System.Windows.Forms.TextBox();
+            this.labelAQuadratic = new System.Windows.Forms.Label();
+            this.labelQuadraticEquation = new System.Windows.Forms.Label();
+            this.groupBoxCubic = new System.Windows.Forms.GroupBox();
+            this.labelCubicEquation = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelCubicResult = new System.Windows.Forms.Label();
+            this.buttonSolveCubic = new System.Windows.Forms.Button();
+            this.textBoxDCubic = new System.Windows.Forms.TextBox();
+            this.labelDCubic = new System.Windows.Forms.Label();
+            this.textBoxCCubic = new System.Windows.Forms.TextBox();
+            this.labelCCubic = new System.Windows.Forms.Label();
+            this.textBoxBCubic = new System.Windows.Forms.TextBox();
+            this.labelBCubic = new System.Windows.Forms.Label();
+            this.textBoxACubic = new System.Windows.Forms.TextBox();
+            this.labelACubic = new System.Windows.Forms.Label();
+            this.buttonHelp = new System.Windows.Forms.Button();
+            this.buttonClearQuadratic = new System.Windows.Forms.Button();
+            this.buttonClearCubic = new System.Windows.Forms.Button();
+            this.groupBoxQuadratic.SuspendLayout();
+            this.groupBoxCubic.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxQuadratic
+            // 
+            this.groupBoxQuadratic.Controls.Add(this.buttonClearQuadratic);
+            this.groupBoxQuadratic.Controls.Add(this.labelQuadraticResult);
+            this.groupBoxQuadratic.Controls.Add(this.buttonSolveQuadratic);
+            this.groupBoxQuadratic.Controls.Add(this.textBoxCQuadratic);
+            this.groupBoxQuadratic.Controls.Add(this.labelCQuadratic);
+            this.groupBoxQuadratic.Controls.Add(this.textBoxBQuadratic);
+            this.groupBoxQuadratic.Controls.Add(this.labelBQuadratic);
+            this.groupBoxQuadratic.Controls.Add(this.textBoxAQuadratic);
+            this.groupBoxQuadratic.Controls.Add(this.labelAQuadratic);
+            this.groupBoxQuadratic.Controls.Add(this.labelQuadraticEquation);
+            this.groupBoxQuadratic.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxQuadratic.Name = "groupBoxQuadratic";
+            this.groupBoxQuadratic.Size = new System.Drawing.Size(562, 180);
+            this.groupBoxQuadratic.TabIndex = 0;
+            this.groupBoxQuadratic.TabStop = false;
+            this.groupBoxQuadratic.Text = "Квадратное уравнение";
+            // 
+            // labelQuadraticResult
+            // 
+            this.labelQuadraticResult.AutoSize = true;
+            this.labelQuadraticResult.Location = new System.Drawing.Point(175, 85);
+            this.labelQuadraticResult.Name = "labelQuadraticResult";
+            this.labelQuadraticResult.Size = new System.Drawing.Size(0, 13);
+            this.labelQuadraticResult.TabIndex = 8;
+            // 
+            // buttonSolveQuadratic
+            // 
+            this.buttonSolveQuadratic.Location = new System.Drawing.Point(18, 132);
+            this.buttonSolveQuadratic.Name = "buttonSolveQuadratic";
+            this.buttonSolveQuadratic.Size = new System.Drawing.Size(75, 23);
+            this.buttonSolveQuadratic.TabIndex = 7;
+            this.buttonSolveQuadratic.Text = "Решить";
+            this.buttonSolveQuadratic.UseVisualStyleBackColor = true;
+            this.buttonSolveQuadratic.Click += new System.EventHandler(this.buttonSolveQuadratic_Click);
+            // 
+            // textBoxCQuadratic
+            // 
+            this.textBoxCQuadratic.Location = new System.Drawing.Point(34, 100);
+            this.textBoxCQuadratic.Name = "textBoxCQuadratic";
+            this.textBoxCQuadratic.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCQuadratic.TabIndex = 6;
+            // 
+            // labelCQuadratic
+            // 
+            this.labelCQuadratic.AutoSize = true;
+            this.labelCQuadratic.Location = new System.Drawing.Point(15, 103);
+            this.labelCQuadratic.Name = "labelCQuadratic";
+            this.labelCQuadratic.Size = new System.Drawing.Size(16, 13);
+            this.labelCQuadratic.TabIndex = 5;
+            this.labelCQuadratic.Text = "c:";
+            // 
+            // textBoxBQuadratic
+            // 
+            this.textBoxBQuadratic.Location = new System.Drawing.Point(34, 74);
+            this.textBoxBQuadratic.Name = "textBoxBQuadratic";
+            this.textBoxBQuadratic.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBQuadratic.TabIndex = 4;
+            // 
+            // labelBQuadratic
+            // 
+            this.labelBQuadratic.AutoSize = true;
+            this.labelBQuadratic.Location = new System.Drawing.Point(15, 77);
+            this.labelBQuadratic.Name = "labelBQuadratic";
+            this.labelBQuadratic.Size = new System.Drawing.Size(16, 13);
+            this.labelBQuadratic.TabIndex = 3;
+            this.labelBQuadratic.Text = "b:";
+            // 
+            // textBoxAQuadratic
+            // 
+            this.textBoxAQuadratic.Location = new System.Drawing.Point(34, 48);
+            this.textBoxAQuadratic.Name = "textBoxAQuadratic";
+            this.textBoxAQuadratic.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAQuadratic.TabIndex = 2;
+            // 
+            // labelAQuadratic
+            // 
+            this.labelAQuadratic.AutoSize = true;
+            this.labelAQuadratic.Location = new System.Drawing.Point(15, 51);
+            this.labelAQuadratic.Name = "labelAQuadratic";
+            this.labelAQuadratic.Size = new System.Drawing.Size(16, 13);
+            this.labelAQuadratic.TabIndex = 1;
+            this.labelAQuadratic.Text = "a:";
+            // 
+            // labelQuadraticEquation
+            // 
+            this.labelQuadraticEquation.AutoSize = true;
+            this.labelQuadraticEquation.Location = new System.Drawing.Point(15, 25);
+            this.labelQuadraticEquation.Name = "labelQuadraticEquation";
+            this.labelQuadraticEquation.Size = new System.Drawing.Size(80, 13);
+            this.labelQuadraticEquation.TabIndex = 0;
+            this.labelQuadraticEquation.Text = "ax² + bx + c = 0";
+            // 
+            // groupBoxCubic
+            // 
+            this.groupBoxCubic.Controls.Add(this.buttonClearCubic);
+            this.groupBoxCubic.Controls.Add(this.labelCubicEquation);
+            this.groupBoxCubic.Controls.Add(this.label1);
+            this.groupBoxCubic.Controls.Add(this.labelCubicResult);
+            this.groupBoxCubic.Controls.Add(this.buttonSolveCubic);
+            this.groupBoxCubic.Controls.Add(this.textBoxDCubic);
+            this.groupBoxCubic.Controls.Add(this.labelDCubic);
+            this.groupBoxCubic.Controls.Add(this.textBoxCCubic);
+            this.groupBoxCubic.Controls.Add(this.labelCCubic);
+            this.groupBoxCubic.Controls.Add(this.textBoxBCubic);
+            this.groupBoxCubic.Controls.Add(this.labelBCubic);
+            this.groupBoxCubic.Controls.Add(this.textBoxACubic);
+            this.groupBoxCubic.Controls.Add(this.labelACubic);
+            this.groupBoxCubic.Location = new System.Drawing.Point(12, 202);
+            this.groupBoxCubic.Name = "groupBoxCubic";
+            this.groupBoxCubic.Size = new System.Drawing.Size(562, 180);
+            this.groupBoxCubic.TabIndex = 1;
+            this.groupBoxCubic.TabStop = false;
+            this.groupBoxCubic.Text = "Кубическое уравнение";
+            // 
+            // labelCubicEquation
+            // 
+            this.labelCubicEquation.AutoSize = true;
+            this.labelCubicEquation.Location = new System.Drawing.Point(13, 20);
+            this.labelCubicEquation.Name = "labelCubicEquation";
+            this.labelCubicEquation.Size = new System.Drawing.Size(106, 13);
+            this.labelCubicEquation.TabIndex = 13;
+            this.labelCubicEquation.Text = "ax³ + bx² + cx + d = 0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 9;
+            // 
+            // labelCubicResult
+            // 
+            this.labelCubicResult.AutoSize = true;
+            this.labelCubicResult.Location = new System.Drawing.Point(175, 85);
+            this.labelCubicResult.Name = "labelCubicResult";
+            this.labelCubicResult.Size = new System.Drawing.Size(0, 13);
+            this.labelCubicResult.TabIndex = 9;
+            // 
+            // buttonSolveCubic
+            // 
+            this.buttonSolveCubic.Location = new System.Drawing.Point(16, 140);
+            this.buttonSolveCubic.Name = "buttonSolveCubic";
+            this.buttonSolveCubic.Size = new System.Drawing.Size(75, 23);
+            this.buttonSolveCubic.TabIndex = 8;
+            this.buttonSolveCubic.Text = "Решить";
+            this.buttonSolveCubic.UseVisualStyleBackColor = true;
+            this.buttonSolveCubic.Click += new System.EventHandler(this.buttonSolveCubic_Click);
+            // 
+            // textBoxDCubic
+            // 
+            this.textBoxDCubic.Location = new System.Drawing.Point(32, 114);
+            this.textBoxDCubic.Name = "textBoxDCubic";
+            this.textBoxDCubic.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDCubic.TabIndex = 7;
+            // 
+            // labelDCubic
+            // 
+            this.labelDCubic.AutoSize = true;
+            this.labelDCubic.Location = new System.Drawing.Point(13, 117);
+            this.labelDCubic.Name = "labelDCubic";
+            this.labelDCubic.Size = new System.Drawing.Size(16, 13);
+            this.labelDCubic.TabIndex = 6;
+            this.labelDCubic.Text = "d:";
+            // 
+            // textBoxCCubic
+            // 
+            this.textBoxCCubic.Location = new System.Drawing.Point(32, 88);
+            this.textBoxCCubic.Name = "textBoxCCubic";
+            this.textBoxCCubic.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCCubic.TabIndex = 5;
+            // 
+            // labelCCubic
+            // 
+            this.labelCCubic.AutoSize = true;
+            this.labelCCubic.Location = new System.Drawing.Point(13, 91);
+            this.labelCCubic.Name = "labelCCubic";
+            this.labelCCubic.Size = new System.Drawing.Size(16, 13);
+            this.labelCCubic.TabIndex = 4;
+            this.labelCCubic.Text = "c:";
+            // 
+            // textBoxBCubic
+            // 
+            this.textBoxBCubic.Location = new System.Drawing.Point(32, 62);
+            this.textBoxBCubic.Name = "textBoxBCubic";
+            this.textBoxBCubic.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBCubic.TabIndex = 3;
+            // 
+            // labelBCubic
+            // 
+            this.labelBCubic.AutoSize = true;
+            this.labelBCubic.Location = new System.Drawing.Point(13, 65);
+            this.labelBCubic.Name = "labelBCubic";
+            this.labelBCubic.Size = new System.Drawing.Size(16, 13);
+            this.labelBCubic.TabIndex = 2;
+            this.labelBCubic.Text = "b:";
+            // 
+            // textBoxACubic
+            // 
+            this.textBoxACubic.Location = new System.Drawing.Point(32, 36);
+            this.textBoxACubic.Name = "textBoxACubic";
+            this.textBoxACubic.Size = new System.Drawing.Size(100, 20);
+            this.textBoxACubic.TabIndex = 1;
+            // 
+            // labelACubic
+            // 
+            this.labelACubic.AutoSize = true;
+            this.labelACubic.Location = new System.Drawing.Point(13, 39);
+            this.labelACubic.Name = "labelACubic";
+            this.labelACubic.Size = new System.Drawing.Size(16, 13);
+            this.labelACubic.TabIndex = 0;
+            this.labelACubic.Text = "a:";
+            // 
+            // buttonHelp
+            // 
+            this.buttonHelp.Location = new System.Drawing.Point(614, 359);
+            this.buttonHelp.Name = "buttonHelp";
+            this.buttonHelp.Size = new System.Drawing.Size(75, 23);
+            this.buttonHelp.TabIndex = 2;
+            this.buttonHelp.Text = "Справка";
+            this.buttonHelp.UseVisualStyleBackColor = true;
+            this.buttonHelp.Click += new System.EventHandler(this.buttonHelp_Click);
+            // 
+            // buttonClearQuadratic
+            // 
+            this.buttonClearQuadratic.Location = new System.Drawing.Point(99, 132);
+            this.buttonClearQuadratic.Name = "buttonClearQuadratic";
+            this.buttonClearQuadratic.Size = new System.Drawing.Size(75, 23);
+            this.buttonClearQuadratic.TabIndex = 13;
+            this.buttonClearQuadratic.Text = "Очистить";
+            this.buttonClearQuadratic.UseVisualStyleBackColor = true;
+            this.buttonClearQuadratic.Click += new System.EventHandler(this.buttonClearQuadratic_Click);
+            // 
+            // buttonClearCubic
+            // 
+            this.buttonClearCubic.Location = new System.Drawing.Point(97, 140);
+            this.buttonClearCubic.Name = "buttonClearCubic";
+            this.buttonClearCubic.Size = new System.Drawing.Size(75, 23);
+            this.buttonClearCubic.TabIndex = 14;
+            this.buttonClearCubic.Text = "Очистить";
+            this.buttonClearCubic.UseVisualStyleBackColor = true;
+            this.buttonClearCubic.Click += new System.EventHandler(this.buttonClearCubic_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 394);
+            this.Controls.Add(this.buttonHelp);
+            this.Controls.Add(this.groupBoxCubic);
+            this.Controls.Add(this.groupBoxQuadratic);
+            this.Name = "Form1";
+            this.Text = "PolySolve";
+            this.groupBoxQuadratic.ResumeLayout(false);
+            this.groupBoxQuadratic.PerformLayout();
+            this.groupBoxCubic.ResumeLayout(false);
+            this.groupBoxCubic.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        #region Квадратное уравнение
+
+        private System.Windows.Forms.GroupBox groupBoxQuadratic;
+        private System.Windows.Forms.Label labelQuadraticResult;
+        private System.Windows.Forms.Button buttonSolveQuadratic;
+        private System.Windows.Forms.TextBox textBoxCQuadratic;
+        private System.Windows.Forms.Label labelCQuadratic;
+        private System.Windows.Forms.TextBox textBoxBQuadratic;
+        private System.Windows.Forms.Label labelBQuadratic;
+        private System.Windows.Forms.TextBox textBoxAQuadratic;
+        private System.Windows.Forms.Label labelAQuadratic;
+        private System.Windows.Forms.Label labelQuadraticEquation;
+
+        #endregion
+
+        #region Кубическое уравнение
+
+        private System.Windows.Forms.GroupBox groupBoxCubic;
+        private System.Windows.Forms.Label labelCubicResult;
+        private System.Windows.Forms.Button buttonSolveCubic;
+        private System.Windows.Forms.TextBox textBoxDCubic;
+        private System.Windows.Forms.Label labelDCubic;
+        private System.Windows.Forms.TextBox textBoxCCubic;
+        private System.Windows.Forms.Label labelCCubic;
+        private System.Windows.Forms.TextBox textBoxBCubic;
+        private System.Windows.Forms.Label labelBCubic;
+        private System.Windows.Forms.TextBox textBoxACubic;
+        private System.Windows.Forms.Label labelACubic;
+
+        #endregion
+
+        private System.Windows.Forms.Label labelCubicEquation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonHelp;
+        private System.Windows.Forms.Button buttonClearQuadratic;
+        private System.Windows.Forms.Button buttonClearCubic;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace PolySolve
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Функция для вычисления кубического корня, корректно обрабатывающая отрицательные числа
+        public static double Cbrt(double x)
+        {
+            if (x >= 0)
+            {
+                return Math.Pow(x, 1.0 / 3.0);
+            }
+            else
+            {
+                return -Math.Pow(-x, 1.0 / 3.0);
+            }
+        }
+
+        private void buttonSolveQuadratic_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            string errorMessage = "";
+
+            if (!double.TryParse(textBoxAQuadratic.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out a))
+            {
+                errorMessage += "Ошибка: Неверный формат числа в поле a.\n";
+            }
+            else if (a == 0)
+            {
+                errorMessage += "Ошибка: a не может быть равно 0. Уравнение становится линейным.\n";
+            }
+
+            if (!double.TryParse(textBoxBQuadratic.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out b))
+            {
+                errorMessage += "Ошибка: Неверный формат числа в поле b.\n";
+            }
+
+            if (!double.TryParse(textBoxCQuadratic.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out c))
+            {
+                errorMessage += "Ошибка: Неверный формат числа в поле c.\n";
+            }
+
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                labelQuadraticResult.Text = errorMessage;
+                return;
+            }
+
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant > 0)
+            {
+                double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+                labelQuadraticResult.Text = $"x1 = {x1:F6}\n x2 = {x2:F6}";
+            }
+            else if (discriminant == 0)
+            {
+                double x = -b / (2 * a);
+                labelQuadraticResult.Text = $"x = {x:F6}";
+            }
+            else
+            {
+                double realPart = -b / (2 * a);
+                double imaginaryPart = Math.Sqrt(Math.Abs(discriminant)) / (2 * a);
+                labelQuadraticResult.Text = $"x1 = {realPart:F6} + {imaginaryPart:F6}i\n x2 = {realPart:F6} - {imaginaryPart:F6}i";
+            }
+        }
+
+        private void buttonSolveCubic_Click(object sender, EventArgs e)
+        {
+            double a, b, c, d;
+            string errorMessage = "";
+
+            if (!double.TryParse(textBoxACubic.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out a))
+            {
+                errorMessage += "Ошибка: Неверный формат числа в поле a.\n";
+            }
+            else if (a == 0)
+            {
+                errorMessage += "Ошибка: a не может быть равно 0. Уравнение становится квадратным.\n";
+            }
+
+            if (!double.TryParse(textBoxBCubic.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out b))
+            {
+                errorMessage += "Ошибка: Неверный формат числа в поле b.\n";
+            }
+
+            if (!double.TryParse(textBoxCCubic.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out c))
+            {
+                errorMessage += "Ошибка: Неверный формат числа в поле c.\n";
+            }
+
+            if (!double.TryParse(textBoxDCubic.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out d))
+            {
+                errorMessage += "Ошибка: Неверный формат числа в поле d.\n";
+            }
+
+            if (!string.IsNullOrEmpty(errorMessage))
+            {
+                labelCubicResult.Text = errorMessage;
+                return;
+            }
+
+            // Нормализация: делим все коэффициенты на 'a'
+            b /= a;
+            c /= a;
+            d /= a;
+            a = 1; // Теперь a = 1
+
+            double Q = (3 * c - b * b) / 9;
+            double R = (9 * b * c - 27 * d - 2 * b * b * b) / 54;
+            double delta = Q * Q * Q + R * R;
+
+            if (delta >= 0)
+            {
+                // Один действительный корень и два комплексных сопряженных (или три действительных, но есть кратные)
+                double S = Cbrt(R + Math.Sqrt(delta));
+                double T = Cbrt(R - Math.Sqrt(delta));
+
+                double x1 = S + T - b / 3;
+                double realPart = -(S + T) / 2 - b / 3;
+                double imagPart = (S - T) * Math.Sqrt(3) / 2;
+
+                labelCubicResult.Text = $"x1 = {x1:F6}\n" +
+                                        $"x2 = {realPart:F6} + {imagPart:F6}i\n" +
+                                        $"x3 = {realPart:F6} - {imagPart:F6}i";
+            }
+            else
+            {
+                // Три действительных корня (неприводимый случай)
+                double theta = Math.Acos(R / Math.Sqrt(-Q * Q * Q));
+
+                double x1 = 2 * Math.Sqrt(-Q) * Math.Cos(theta / 3) - b / 3;
+                double x2 = 2 * Math.Sqrt(-Q) * Math.Cos((theta + 2 * Math.PI) / 3) - b / 3;
+                double x3 = 2 * Math.Sqrt(-Q) * Math.Cos((theta + 4 * Math.PI) / 3) - b / 3;
+
+                labelCubicResult.Text = $"x1 = {x1:F6}\n x2 = {x2:F6}\n x3 = {x3:F6}";
+            }
+        }
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            string helpText = "Программа PolySolve решает квадратные и кубические уравнения.\n\n" +
+                              "**Квадратные уравнения:**\n" +
+                              "Введите коэффициенты a, b и c в соответствующие поля.\n" +
+                              "Уравнение имеет вид: ax² + bx + c = 0\n\n" +
+                              "**Кубические уравнения:**\n" +
+                              "Введите коэффициенты a, b, c и d в соответствующие поля.\n" +
+                              "Уравнение имеет вид: ax³ + bx² + cx + d = 0\n\n" +
+                              "**Формат ввода чисел:**\n" +
+                              "Используйте точку (.) в качестве десятичного разделителя.\n\n" +
+                              "**Отображение корней:**\n" +
+                              "Программа вычисляет все корни (действительные и комплексные) для квадратных и кубических уравнений.";
+
+            MessageBox.Show(helpText, "Использование программы PolySolve", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void buttonClearQuadratic_Click(object sender, EventArgs e)
+        {
+            // Очистка полей ввода для квадратных уравнений
+            textBoxAQuadratic.Text = "";
+            textBoxBQuadratic.Text = "";
+            textBoxCQuadratic.Text = "";
+            labelQuadraticResult.Text = "";
+        }
+
+        private void buttonClearCubic_Click(object sender, EventArgs e)
+        {
+            // Очистка полей ввода для кубических уравнений
+            textBoxACubic.Text = "";
+            textBoxBCubic.Text = "";
+            textBoxCCubic.Text = "";
+            textBoxDCubic.Text = "";
+            labelCubicResult.Text = "";
+        }
+    }
+}
